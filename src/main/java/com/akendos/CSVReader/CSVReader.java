72b6db58598eff7ca4c4ddb9aeeae3cc8e39b76a
Giovanny <giovanny.sayas@akendos.com>
2018-08-11 18:27:22 +0100

    load file

--- src/main/java/com/akendos/CSVReader/CSVReader.java
+++ src/main/java/com/akendos/CSVReader/CSVReader.java
@@ -1,4 +1,46 @@
 package com.akendos.CSVReader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVReader {
+
+    private String csvFile;
+    private String cvsSplitBy;
+    private List<String> lines;
+
+    public CSVReader(String filePath) {
+        csvFile = filePath;
+        cvsSplitBy = ",";
+        lines = new ArrayList<>();
+    }
+
+    public void loadFile() {
+        String line = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                lines.add(line);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getNumRows() {
+        return lines.size();
+    }
+
+    public int getNumCols() {
+        return 0;
+    }
+
 }
--- src/test/java/CSVReaderTest.java
+++ src/test/java/CSVReaderTest.java
@@ -1,15 +1,26 @@
 import com.akendos.CSVReader.CSVReader;
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
 public class CSVReaderTest {
-    @Test
-    public void testReadLine() {
-        CSVReader reader = new CSVReader();
 
-        assertEquals("", "");
-        
+    private CSVReader reader;
+
+    @Before
+    public void setUp() throws Exception {
+        reader = new CSVReader("/home/giovanny/IdeaProjects/InvoiceApp/example.csv");
+
+    }
+
+    @Test
+    public void testLoadFile() {
+        reader.loadFile();
+
+        assertEquals(3, reader.getNumRows());
+        //assertEquals(2, reader.getNumCols());
+
     }
 
 }
